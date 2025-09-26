71943b1890be8633d2952816d3e84e88918dd4ac
JimmyWu <workjimmywu0820@gmail.com>
2025-09-26 10:00:46 +0800

    新增indexController註解

--- src/main/java/com/farmtastic/IndexController.java
+++ src/main/java/com/farmtastic/IndexController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
+	
+	// 這是測試用網頁
 	@GetMapping("/")
 	public String indexTest() {
 		return "/front_end/index";
