adfc64cac2d6752469e3121b442c38035a712c55
JimmyWu <workjimmywu0820@gmail.com>
2025-09-27 23:35:26 +0800

    新增IndexController的 @GetMapping("/cashflow")

--- src/main/java/com/farmtastic/IndexController.java
+++ src/main/java/com/farmtastic/IndexController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
-	
 	// 這是測試用網頁
 	@GetMapping("/")
 	public String indexTest() {
 		return "/front_end/index";
+	}
+	
+	@GetMapping("/cashflow")
+	public String listAllProOrder() {
+		return "/back_end/logined/cash_flow/listAllEmp";
 	}
 }
