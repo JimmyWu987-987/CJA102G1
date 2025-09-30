e7f1b19b16dd17b81c1f189b2021f0b12374808d
JimmyWu <workjimmywu0820@gmail.com>
2025-09-30 10:15:24 +0800

    完成查詢訂單的修改（詳細資料）按鈕

--- src/main/java/com/farmtastic/proorder/controller/ProOrderController.java
+++ src/main/java/com/farmtastic/proorder/controller/ProOrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.farmtastic.proorder.model.ProOrderSevice;
@@ -18,6 +19,7 @@ public class ProOrderController {
 	@Autowired
 	ProOrderSevice proOrdSvc;
 	
+	// 查詢全部訂單
 	@GetMapping("/listAllProOrder")
 	public String listAll(Model model) {
 	
@@ -27,5 +29,18 @@ public class ProOrderController {
 
         return "/back_end/logined/cash_flow/listAllProOrder";
 	}
+	
+	// 查詢單筆訂單
+	@PostMapping("/listOneProOrder")
+	public String listOne(Model model) {
+		
+		List<ProOrderVO> list = proOrdSvc.getAll();
+		
+		model.addAttribute("proOrderOne",list);
+		
+		return "/back_end/logined/cash_flow/listAllProOrder";
+	}
+	
+	
 	
 }
