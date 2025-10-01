aeeab80b002660505aa8c9abd1a48a3aaeb5aa8c
JimmyWu <workjimmywu0820@gmail.com>
2025-10-01 14:50:45 +0800

    儲存查詢訂單的表單

--- src/main/java/com/farmtastic/proorder/controller/ProOrderMemIdController.java
+++ src/main/java/com/farmtastic/proorder/controller/ProOrderMemIdController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.farmtastic.member.model.Mem;
 import com.farmtastic.proorder.model.ProOrderSevice;
@@ -15,7 +18,7 @@ import com.farmtastic.proorder.model.ProOrderVO;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/mem")
+@RequestMapping("/mem/proorders")
 public class ProOrderMemIdController {
 	
 	@Autowired
@@ -23,10 +26,11 @@ public class ProOrderMemIdController {
 	
 	
 	// 查詢該會員的全部訂單
-	@GetMapping("ProOrdersByMem")
+	@GetMapping("listAllProOrder")
 	public String listAll(Model model,HttpSession session) {
 		
 		// 取得會員資訊
+		
 		Mem MemVO = new Mem();
 		
 		// 測試 會員ID為1的會員
@@ -34,21 +38,21 @@ public class ProOrderMemIdController {
 		
 		List<ProOrderVO> list = proOrdSvc.getAllByMemId(MemVO);
 		
-		model.addAttribute("proOrderListByMemID",list);
+		model.addAttribute("proOrderList",list);
 
         return "/front_end/customer/logined/memProOrders/listAllProOrder";
 	}
 	
 	// 查詢單筆訂單
-//	@PostMapping("listOneProOrder")
-//	public String listOneProOrder(@RequestParam("proOrdId") String proOrdId, ModelMap model) {
-//		
-//		ProOrderVO proOrderVO = proOrdSvc.getOneProOrder(Integer.valueOf(proOrdId));
-//		
-//		model.addAttribute("proOrderVO",proOrderVO);
-//		
-//		return "/back_end/logined/cash_flow/listOneProOrder";
-//	}
+	@PostMapping("listOneProOrder")
+	public String listOneProOrder(@RequestParam("proOrdId") String proOrdId, ModelMap model) {
+		
+		ProOrderVO proOrderVO = proOrdSvc.getOneProOrder(Integer.valueOf(proOrdId));
+		
+		model.addAttribute("proOrderVO",proOrderVO);
+		
+		return "/back_end/logined/cash_flow/listOneProOrder";
+	}
 	
 	
 	
