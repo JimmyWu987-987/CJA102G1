92b1dbdfcbe0df207dc487d1e88012acc6c6c68e
JimmyWu <workjimmywu0820@gmail.com>
2025-10-02 13:28:27 +0800

    關於Conroller的路徑說明

--- src/main/java/com/farmtastic/IndexController.java
+++ src/main/java/com/farmtastic/IndexController.java
@@ -13,19 +13,6 @@ public class IndexController {
 		return "/front_end/index";
 	}
 	
-	// 一般會員的商品訂單頁面
-	@GetMapping("/mem/proorders")
-	public String memProOrders() {
-		return "/front_end/customer/logined/memProOrders/index";
-	}
-	
-	// 金流管理首頁
-	@GetMapping("/admin/cashflow/index")
-	public String cashflowIndex() {
-		return "/back_end/logined/cash_flow/index";
-	}
-	
-
 	
 //	@GetMapping("/mem/showMemRegLoginForm")
 //	public String memRegLogin() {
