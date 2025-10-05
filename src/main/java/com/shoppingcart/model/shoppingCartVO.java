26f86bffd6cb71d9e6158bd2a90aae75cee747b8
JimmyWu <workjimmywu0820@gmail.com>
2025-10-05 11:43:08 +0800

    購物車VO 新增Serializable

--- src/main/java/com/shoppingcart/model/shoppingCartVO.java
+++ src/main/java/com/shoppingcart/model/shoppingCartVO.java
@@ -1,6 +1,11 @@
 package com.shoppingcart.model;
 
-public class shoppingCartVO {
+import java.io.Serializable;
+
+public class shoppingCartVO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
 	private Integer memId;
 	private Integer proId;
 	private String cartName;
