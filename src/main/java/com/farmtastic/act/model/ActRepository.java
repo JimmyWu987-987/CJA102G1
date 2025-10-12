cc66e2ee8eac57dadba9987fd063561c37eb5228
Jamie <yukige616@gmail.com>
2025-10-12 17:56:45 +0800

    修正程式碼

--- src/main/java/com/farmtastic/act/model/ActRepository.java
+++ src/main/java/com/farmtastic/act/model/ActRepository.java
@@ -22,13 +22,15 @@ public interface ActRepository extends JpaRepository<Act, Integer> {
     List<Act> findByFmemIdAndActStat(Integer fmemId, Integer actStat, Sort sort);
 
     // 複合查詢(用Repository + Sort/JPQL), 練習不用原生sql...
-    @Query("SELECT DISTINCT a FROM act a " +
+    @Query("SELECT DISTINCT a FROM Act a " +
            "JOIN a.actCate ac " +
            "WHERE (:fmemId IS NULL OR a.fmemId = :fmemId) " +
            "AND (:actStat IS NULL OR a.actStat = :actStat) " +
            "AND (:actLaunStat IS NULL OR a.actLaunStat = :actLaunStat) " +
            "AND (:actcateId IS NULL OR ac.actCateId = :actcateId) " +
-           "AND (:keyword IS NULL OR a.actName LIKE %:keyword% OR a.actDes LIKE %:keyword%)")
+           "AND (:keyword IS NULL OR a.actName LIKE CONCAT('%', :keyword, '%') " +
+           "OR a.actDes LIKE CONCAT('%', :keyword, '%'))")
+    
     List<Act> findActByCQ(@Param("fmemId") Integer fmemId,
                                @Param("actStat") Integer actStat,
                                @Param("actLaunStat") Integer actLaunStat,
@@ -38,6 +40,6 @@ public interface ActRepository extends JpaRepository<Act, Integer> {
 
     @Transactional
     @Modifying
-    @Query("DELETE FROM act a WHERE a.actId = :actId")
+    @Query("DELETE FROM Act a WHERE a.actId = :actId")
     void deleteByActId(@Param("actId") Integer actId);
 }
