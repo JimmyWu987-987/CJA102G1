02c6d033a4b9875ad4ab142f11efef72fb521490
Jamie <yukige616@gmail.com>
2025-10-12 15:42:01 +0800

    修改變數大小寫&調排版

--- src/main/java/com/farmtastic/ses/model/Ses.java
+++ src/main/java/com/farmtastic/ses/model/Ses.java
@@ -18,179 +18,179 @@ import jakarta.persistence.Table;
 @Table(name = "ses")
 public class Ses {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ses_id")
-    private Integer sesId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ses_id")
+	private Integer sesId;
 
-    @Column(name = "ses_date", nullable = false)
-    private LocalDate sesDate;
+	@Column(name = "ses_date", nullable = false)
+	private LocalDate sesDate;
 
-    @Column(name = "ses_start", nullable = false)
-    private LocalTime sesStart;
+	@Column(name = "ses_start", nullable = false)
+	private LocalTime sesStart;
 
-    @Column(name = "ses_end", nullable = false)
-    private LocalTime sesEnd;
+	@Column(name = "ses_end", nullable = false)
+	private LocalTime sesEnd;
 
-    @Column(name = "reg_start", nullable = false)
+	@Column(name = "reg_start", nullable = false)
     private LocalDate regStart;
 
-    @Column(name = "reg_end", nullable = false)
-    private LocalDate regEnd;
+	@Column(name = "reg_end", nullable = false)
+	private LocalDate regEnd;
 
-    @Column(name = "minppl", nullable = false)
-    private Integer minppl = 1;		// 預設1
+	@Column(name = "minppl", nullable = false)
+	private Integer minPpl = 1;		// 預設1
 
-    @Column(name = "maxppl", nullable = false)
-    private Integer maxppl;
+	@Column(name = "maxppl", nullable = false)
+	private Integer maxPpl;
 
-    @Column(name = "ses_fee", nullable = false)
-    private Integer sesFee;
+	@Column(name = "ses_fee", nullable = false)
+	private Integer sesFee;
 
-    @Column(name = "notice", nullable = false)
-    private Integer notice = 1;		// 預設1天前
+	@Column(name = "notice", nullable = false)
+	private Integer notice = 1;		// 預設1天前
 
-    @Column(name = "ses_launstat", nullable = false)
-    private Integer sesLaunStat = 0;	// 預設0 (下架)
+	@Column(name = "ses_launstat", nullable = false)
+	private Integer sesLaunStat = 0;	// 預設0 (下架)
 
-    @Column(name = "reg_stat", nullable = false)
-    private Integer regStat = 0; // 預設正常
+	@Column(name = "reg_stat", nullable = false)
+	private Integer regStat = 0; // 預設正常
 
-    @Column(name = "ses_launupd")
-    private LocalDateTime sesLaunupd;
+	@Column(name = "ses_launupd")
+	private LocalDateTime sesLaunUpd;
 
-    @Column(name = "headcount")
-    private Integer headcount = 0;	// 預設0
+	@Column(name = "headcount")
+	private Integer headCount = 0;	// 預設0
 
-    @Column(name = "act_id", nullable = false)
-    private Integer actId;
+	@Column(name = "act_id", nullable = false)
+	private Integer actId;
 
-    public Integer getSesId() {
-        return sesId;
-    }
+	public Integer getSesId() {
+		return sesId;
+	}
 
-    public void setSesId(Integer sesId) {
-        this.sesId = sesId;
-    }
+	public void setSesId(Integer sesId) {
+		this.sesId = sesId;
+	}
 
-    public LocalDate getSesDate() {
-        return sesDate;
-    }
+	public LocalDate getSesDate() {
+		return sesDate;
+	}
 
-    public void setSesDate(LocalDate sesDate) {
-        this.sesDate = sesDate;
-    }
+	public void setSesDate(LocalDate sesDate) {
+		this.sesDate = sesDate;
+	}
 
-    public LocalTime getSesStart() {
-        return sesStart;
-    }
+	public LocalTime getSesStart() {
+		return sesStart;
+	}
 
-    public void setSesStart(LocalTime sesStart) {
-        this.sesStart = sesStart;
-    }
+	public void setSesStart(LocalTime sesStart) {
+		this.sesStart = sesStart;
+	}
 
-    public LocalTime getSesEnd() {
-        return sesEnd;
-    }
+	public LocalTime getSesEnd() {
+		return sesEnd;
+	}
 
-    public void setSesEnd(LocalTime sesEnd) {
-        this.sesEnd = sesEnd;
-    }
+	public void setSesEnd(LocalTime sesEnd) {
+		this.sesEnd = sesEnd;
+	}
 
-    public LocalDate getRegStart() {
-        return regStart;
-    }
+	public LocalDate getRegStart() {
+		return regStart;
+	}
 
-    public void setRegStart(LocalDate regStart) {
-        this.regStart = regStart;
-    }
+	public void setRegStart(LocalDate regStart) {
+		this.regStart = regStart;
+	}
 
-    public LocalDate getRegEnd() {
-        return regEnd;
-    }
+	public LocalDate getRegEnd() {
+		return regEnd;
+	}
 
-    public void setRegEnd(LocalDate regEnd) {
-        this.regEnd = regEnd;
-    }
+	public void setRegEnd(LocalDate regEnd) {
+		this.regEnd = regEnd;
+	}
 
-    public Integer getMinppl() {
-        return minppl;
-    }
+	public Integer getMinPpl() {
+		return minPpl;
+	}
 
-    public void setMinppl(Integer minppl) {
-        this.minppl = minppl;
-    }
+	public void setMinPpl(Integer minPpl) {
+		this.minPpl = minPpl;
+	}
 
-    public Integer getMaxppl() {
-        return maxppl;
-    }
+	public Integer getMaxPpl() {
+		return maxPpl;
+	}
 
-    public void setMaxppl(Integer maxppl) {
-        this.maxppl = maxppl;
-    }
+	public void setMaxppl(Integer maxPpl) {
+		this.maxPpl = maxPpl;
+	}
 
-    public Integer getSesFee() {
-        return sesFee;
-    }
+	public Integer getSesFee() {
+		return sesFee;
+	}
 
-    public void setSesFee(Integer sesFee) {
-        this.sesFee = sesFee;
-    }
+	public void setSesFee(Integer sesFee) {
+		this.sesFee = sesFee;
+	}
 
-    public Integer getNotice() {
-        return notice;
-    }
+	public Integer getNotice() {
+		return notice;
+	}
 
-    public void setNotice(Integer notice) {
-        this.notice = notice;
-    }
-
-    
-    // 上下架
-    public Integer getSesLaunStat() {
-        return sesLaunStat;
-    }
-
-    public void setSesLaunstat(Integer sesLaunStat) {
-        this.sesLaunStat = sesLaunStat;
-    }
+	public void setNotice(Integer notice) {
+		this.notice = notice;
+	}
 
     
+	// 上下架
+	public Integer getSesLaunStat() {
+		return sesLaunStat;
+	}
+
+	public void setSesLaunstat(Integer sesLaunStat) {
+		this.sesLaunStat = sesLaunStat;
+	}
+
     
-    // 報名狀態
-    public Integer getRegStat() {
-        return regStat;
-    }
+    
+	// 報名狀態
+	public Integer getRegStat() {
+		return regStat;
+	}
 
-    public void setRegStat(Integer regStat) {
-        this.regStat = regStat;
-    }
+	public void setRegStat(Integer regStat) {
+		this.regStat = regStat;
+	}
     
     
 
-    public LocalDateTime getSesLaunupd() {
-        return sesLaunupd;
-    }
+	public LocalDateTime getSesLaunUpd() {
+		return sesLaunUpd;
+	}
 
-    public void setSesLaunupd(LocalDateTime sesLaunupd) {
-        this.sesLaunupd = sesLaunupd;
-    }
+	public void setSesLaunupd(LocalDateTime sesLaunUpd) {
+		this.sesLaunUpd = sesLaunUpd;
+	}
 
-    public Integer getHeadcount() {
-        return headcount;
-    }
+	public Integer getHeadCount() {
+		return headCount;
+	}
 
-    public void setHeadcount(Integer headcount) {
-        this.headcount = headcount;
-    }
+	public void setHeadcount(Integer headCount) {
+		this.headCount = headCount;
+	}
 
-    public Integer getActId() {
-        return actId;
-    }
+	public Integer getActId() {
+		return actId;
+	}
 
-    public void setActId(Integer actId) {
-        this.actId = actId;
-    }
+	public void setActId(Integer actId) {
+		this.actId = actId;
+	}
 	
 	// 拿報名狀態文字
 	public String getSesStatText() {
@@ -198,8 +198,8 @@ public class Ses {
 	}
 	
 	// 拿上下架狀態文字
-		public String getLaunStatText() {
-			return LaunStat.getLaunStatDesc(this.sesLaunStat);
-		}
+	public String getLaunStatText() {
+		return LaunStat.getLaunStatDesc(this.sesLaunStat);
+	}
 	
 }
