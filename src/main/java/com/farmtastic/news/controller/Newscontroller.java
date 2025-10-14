ec429ed01805a048be3f83c9ee2cb9587ac0efd0
Mike <bit9023@gmail.com>
2025-10-14 20:38:21 +0800

    feat: 修正商品controller，VO新增最新消息、蕭農個人商品頁面

---
+++ src/main/java/com/farmtastic/news/controller/Newscontroller.java
@@ -0,0 +1,127 @@
+package com.farmtastic.news.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.farmtastic.news.model.News;
+import com.farmtastic.news.model.NewsService;
+
+
+@Controller
+public class Newscontroller {
+
+	@Autowired
+	private NewsService newsService;
+
+	/**
+	 * 顯示所有最新消息列表
+	 * 
+	 * @param model 用於將資料傳遞給視圖
+	 * @return 消息列表頁面的模板名稱
+	 */
+	@GetMapping("/news")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listNews", newsService.getAllNews());
+		return "front_end/customer/unlogined/news/news_list";
+	}
+
+	/**
+	 * 顯示新增消息的表單頁面
+	 * 
+	 * @param model 用於將空的 News 物件綁定到表單
+	 * @return 新增消息表單的模板名稱
+	 */
+	@GetMapping("/newnewsform")
+	public String newNewsForm(Model model) {
+		News news = new News();
+		model.addAttribute("news", news);
+		return "add_news";
+	}
+
+	/**
+	 * 儲存新的消息
+	 * 
+	 * @param News 從表單綁定的 News 物件
+	 * @return 重定向到首頁
+	 */
+	@PostMapping("/savenews")
+	public String saveNews(@ModelAttribute("news") News news) {
+		newsService.saveNews(news);
+		return "redirect:/news";
+	}
+
+	/**
+	 * 顯示修改消息的表單頁面
+	 * 
+	 * @param id    要修改的消息 ID
+	 * @param model 用於將找到的消息資料傳遞給視圖
+	 * @return 修改消息表單的模板名稱
+	 */
+	@GetMapping("/editnews/{id}")
+	public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+		// 從 service 取得 News
+		News news = newsService.getNewsById(id);
+		// 將 News 設定為 model attribute 來預填表單
+		model.addAttribute("news", news);
+		return "front_end/customer/unlogined/news/edit_news";
+	}
+
+	/**
+	 * 更新消息
+	 * 
+	 * @param id   要更新的消息 ID
+	 * @param News 從表單綁定的 News 物件
+	 * @return 重定向到首頁
+	 */
+	@PostMapping("/updatenews/{id}")
+	public String updateNews(@PathVariable("id") long id, @ModelAttribute("News") News news) {
+		News existingNews = newsService.getNewsById(id);
+		existingNews.setNewsTitle(news.getNewsTitle());
+		existingNews.setNewsContent(news.getNewsContent());
+        existingNews.setNewsStatus(news.getNewsStatus()); 
+		existingNews.setNewsAt(LocalDateTime.now()); // 更新發布時間為現在
+		newsService.saveNews(existingNews);
+		return "redirect:/news";
+	}
+
+	/**
+	 * 刪除消息
+	 * 
+	 * @param id 要刪除的消息 ID
+	 * @return 重定向到首頁
+	 */
+	@GetMapping("/deletenews/{id}")
+	public String deleteNews(@PathVariable(value = "id") long id) {
+		this.newsService.deleteNewsById(id);
+		return "redirect:/news";
+	}
+	
+    @GetMapping("/fmem/forfnews")
+    public String showFarmerNews(Model model) {
+        // 狀態 0 代表「小農」
+        model.addAttribute("farmerNewsList", newsService.getNewsByStatus(0));
+        return "for_f_news";
+    }
+    
+    @GetMapping("/forcusnews")
+    public String showCusNews(Model model) {
+        // 狀態 1 代表「消費者」
+        model.addAttribute("cusNewsList", newsService.getNewsByStatus(1));
+        return "front_end/customer/unlogined/news/for_c_news";
+    }
+    
+    //文章
+    @GetMapping("/news/{id}")
+    public String showNewsDetail(@PathVariable("id") long id, Model model) {
+        News news = newsService.getNewsById(id);
+        model.addAttribute("news", news);
+        return "front_end/customer/unlogined/news/newsdetail";
+    }
+}
---
+++ src/main/java/com/farmtastic/news/model/News.java
@@ -0,0 +1,81 @@
+package com.farmtastic.news.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+//import lombok.Data;
+
+//@Data
+@Entity
+@Table(name = "News")
+public class News {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "news_id")
+	private Long id;
+
+	@Column(name = "news_title", nullable = false)
+	private String newsTitle;
+
+	@Column(name = "news_cont", columnDefinition = "TEXT")
+	private String newsContent;
+
+	@Column(name = "news_at")
+	private LocalDateTime newsAt;
+	
+    @Column(name = "news_status")
+    private Integer newsStatus; // 0=小農, 1=消費者
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNewsTitle() {
+		return newsTitle;
+	}
+
+	public void setNewsTitle(String newsTitle) {
+		this.newsTitle = newsTitle;
+	}
+
+	public String getNewsContent() {
+		return newsContent;
+	}
+
+	public void setNewsContent(String newsContent) {
+		this.newsContent = newsContent;
+	}
+
+	public LocalDateTime getNewsAt() {
+		return newsAt;
+	}
+
+	public void setNewsAt(LocalDateTime newsAt) {
+		this.newsAt = newsAt;
+	}
+
+	public Integer getNewsStatus() {
+		return newsStatus;
+	}
+
+	public void setNewsStatus(Integer newsStatus) {
+		this.newsStatus = newsStatus;
+	}
+
+	public News() {
+		super();
+	}
+    
+    
+
+}
