06e5c4677dbfd16c1c8bd87bdbff7a2abd8bfd24
JimmyWu <workjimmywu0820@gmail.com>
2025-10-14 10:57:28 +0800

    為了能啟動專案，刪除同學DAO裡有跟我的測試商品ＶＯ的依賴（有先告知），

--- src/main/java/com/farmtastic/favopro/controller/ProFavotController.java
+++ src/main/java/com/farmtastic/favopro/controller/ProFavotController.java
@@ -8,17 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.farmtastic.shoppingcart.model.Product;
-import com.farmtastic.shoppingcart.model.ProductService;
+import com.farmtastic.pro.model.Pro;
+import com.farmtastic.pro.model.ProService;
 
 //測試收藏之商品
 @Controller
 @RequestMapping("/favo/products")
 public class ProFavotController {
-	private final ProductService productService; // <--- 新增 ProductService 欄位
+	private final ProService productService; // <--- 新增 ProductService 欄位
 
 	@Autowired
-	public ProFavotController(ProductService productService) { // <--- 建構子注入
+	public ProFavotController(ProService productService) { // <--- 建構子注入
 		this.productService = productService;
 	}
 
@@ -28,7 +28,7 @@ public class ProFavotController {
 	@GetMapping("/list")
 	public String listProducts(Model model) {
 		// 透過 ProductService 取得商品清單
-		List<Product> products = productService.getAllProducts();
+		List<Pro> products = productService.getAll();
 
 		model.addAttribute("products", products);
 
