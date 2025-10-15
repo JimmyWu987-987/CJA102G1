f655b74451591838b12355c26e559bb7f0109e7d
JimmyWu <workjimmywu0820@gmail.com>
2025-10-15 17:55:23 +0800

    先將折價卷得部分註解

--- src/main/java/com/farmtastic/proorder/controller/ProOrderMemController.java
+++ src/main/java/com/farmtastic/proorder/controller/ProOrderMemController.java
@@ -407,43 +407,42 @@ public class ProOrderMemController {
 			hasError = true;
 		}
 
-		// ==============會員折價卷數錯誤驗證==============
-		// 取會員所選取的折價卷物件
-		Optional<MemProCpnVO> tempMpcVO = mpcRepository.findById(cpnHolderDetailId);
-		MemProCpnVO mpcVO = tempMpcVO.orElse(new MemProCpnVO());
-		// 取該折價卷的所屬種類（扣金額or百分比）
-		BigDecimal mpcDisc = null;
-		Integer finalMpcDisc = 0;
-		
-		
-		if (cpnHolderDetailId == 0 && tempProOrdPointdisc == 0) {
-			// 0 為未選取折假劵，所以不更新。
-			model.addAttribute("cpnError", "未選擇折價卷！");
-			hasError = true;
-		} else {
-			mpcDisc = mpcVO.getProCpnVO().getDiscValue();
-			
-			switch ((mpcVO.getProCpnVO().getDiscType())) {
-			// 金額折抵
-			case FULL_REDUCTION:
-				// 折價卷折抵後的金額
-				finalMpcDisc = Integer.valueOf(mpcDisc.intValue());
-				break;
-			// 百分比折抵
-			case PERCENTAGE:
-				// 折價卷折抵後的金額
-				finalMpcDisc = proTotal * mpcDisc.intValue();
-				break;
-			default:
-				break;
-			}
-		}
-
+//		// ==============會員折價卷數錯誤驗證==============
+//		// 取會員所選取的折價卷物件
+//		Optional<MemProCpnVO> tempMpcVO = mpcRepository.findById(cpnHolderDetailId);
+//		MemProCpnVO mpcVO = tempMpcVO.orElse(new MemProCpnVO());
+//		// 取該折價卷的所屬種類（扣金額or百分比）
+//		BigDecimal mpcDisc = null;
+//		Integer finalMpcDisc = 0;
+//		
+//		
+//		if (cpnHolderDetailId == 0 && tempProOrdPointdisc == 0) {
+//			// 0 為未選取折假劵，所以不更新。
+//			model.addAttribute("cpnError", "未選擇折價卷！");
+//			hasError = true;
+//		} else {
+//			mpcDisc = mpcVO.getProCpnVO().getDiscValue();
+//			
+//			switch ((mpcVO.getProCpnVO().getDiscType())) {
+//			// 金額折抵
+//			case FULL_REDUCTION:
+//				// 折價卷折抵後的金額
+//				finalMpcDisc = Integer.valueOf(mpcDisc.intValue());
+//				break;
+//			// 百分比折抵
+//			case PERCENTAGE:
+//				// 折價卷折抵後的金額
+//				finalMpcDisc = proTotal * mpcDisc.intValue();
+//				break;
+//			default:
+//				break;
+//			}
+//		}
 		// 如果有邏輯錯誤，返回原頁面
 		if (hasError) {
 			// 儲存新的欄位狀態(保存已選取的欄位狀態)
-			mpcVO.setCpnHolderDetailId(cpnHolderDetailId);
-			finalProOrderVO.setMemProCpnVO(mpcVO);
+//			mpcVO.setCpnHolderDetailId(cpnHolderDetailId);
+//			finalProOrderVO.setMemProCpnVO(mpcVO);
 			// 由於我們是手動返回頁面，必須確保將用戶輸入的值也放回 Model
 			model.addAttribute("inputPointDisc", tempProOrdPointdisc);
 			// 取得會員的有效折價卷資料
@@ -463,7 +462,7 @@ public class ProOrderMemController {
 
 		// (2) 計算新的實付金額 (Grand Total)
 		Integer proOrdShipFee = finalProOrderVO.getProOrdShipFee() != null ? finalProOrderVO.getProOrdShipFee() : 0;
-		Integer proOrdGrandTotal = proTotal + proOrdShipFee - pointCpndisc - pointDiscountAmount - finalMpcDisc;
+		Integer proOrdGrandTotal = proTotal + proOrdShipFee - pointCpndisc - pointDiscountAmount; // - finalMpcDisc;
 		finalProOrderVO.setProOrdGrandTotal(proOrdGrandTotal);
 
 		// (3) 計算新的回饋點數
@@ -471,10 +470,10 @@ public class ProOrderMemController {
 		finalProOrderVO.setProOrdPointGet(proOrdPointGet);
 
 		// (4) 儲存新的欄位狀態(保存已選取的欄位狀態)
-		MemProCpnVO finalMpcVO = mpcVO;
-		finalMpcVO.setCpnHolderDetailId(cpnHolderDetailId);
-		finalProOrderVO.setMemProCpnVO(finalMpcVO);
-		finalProOrderVO.setProOrdCpndisc(finalMpcDisc);
+//		MemProCpnVO finalMpcVO = mpcVO;
+//		finalMpcVO.setCpnHolderDetailId(cpnHolderDetailId);
+//		finalProOrderVO.setMemProCpnVO(finalMpcVO);
+//		finalProOrderVO.setProOrdCpndisc(finalMpcDisc);
 
 		// 5. 將更新後的訂單物件存回 Session
 		session.setAttribute("cartToProOrder", finalProOrderVO);
