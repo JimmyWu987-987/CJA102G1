36d756a0f03844eb964ff79a92594561716ddf71
CHEN YITING <dracomi0156@gmail.com>
2025-10-15 17:40:39 +0800

    修改CSS

--- src/main/java/com/farmtastic/fmember/controller/FmemController.java
+++ src/main/java/com/farmtastic/fmember/controller/FmemController.java
@@ -112,7 +112,6 @@ public class FmemController{
 			List<Pro> proList = proSvc.findByFmemId(fmem.getFmemId());
 			model.addAttribute("proList", proList);
 			
-
 		} else {
 			return "front_end/farmer/unlogined/fmemRegLogin";
 		}
