a00e154e89f02ee0e9931f8c1e7c5fb1a87552b6
JimmyWu <workjimmywu0820@gmail.com>
2025-10-20 13:35:02 +0800

    儲存點

--- src/main/java/com/farmtastic/procom/controller/ProComController.java
+++ src/main/java/com/farmtastic/procom/controller/ProComController.java
@@ -1,18 +1,15 @@
 package com.farmtastic.procom.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.farmtastic.member.model.Mem;
 import com.farmtastic.member.model.MemService;
-import com.farmtastic.procom.dto.ProComByFmemIdDTO;
 import com.farmtastic.procom.model.ProComService;
 import com.farmtastic.procom.model.ProComVO;
 
@@ -33,7 +30,9 @@ public class ProComController {
 	}
 	
 	@PostMapping("add")
-	public String addProCom(Model model,@RequestParam("proOrdId") String proOrdId) {
+	public String addProCom(@RequestParam("proOrdId") String proOrdId,ModelMap model) {
+		ProComVO proComVO = new ProComVO();
+		model.addAttribute("proComVO", proComVO);
 		return "/front_end/customer/logined/proCom/addProCom";
 	}
 	
