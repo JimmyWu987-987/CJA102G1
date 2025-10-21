07fa382b652a3f2cb3b4c4a072b76953c19b7932
WA-LI <orz9527487@gmail.com>
2025-10-21 14:14:13 +0800

    更新註解

--- src/main/java/com/farmtastic/actcpn/controller/ActCpnAdminController.java
+++ src/main/java/com/farmtastic/actcpn/controller/ActCpnAdminController.java
@@ -155,7 +155,7 @@ public class ActCpnAdminController {
 	public String filterCpns(
 			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
 			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end, Model model) {
-		// 1.呼叫日期SERVICE
+		// 1.呼叫日期 SERVICE
 		List<ActCpnVO> filteredList = actCpnSvc.filterByDateRange(start, end);
 		// 2.日期
 		if (start == null && end == null) {
